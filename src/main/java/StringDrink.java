989c9f65c358b453dee16f71e5febc4bf44d91fc
up202004735 <up202004735@up.pt>
2021-12-03 14:42:57 +0000

    second commit

---
+++ src/main/java/StringDrink.java
@@ -0,0 +1,16 @@
+public class StringDrink {
+
+    private String drink;
+
+    public StringDrink(String drink){
+        this.drink = drink;
+    }
+
+    public String getText() {
+        return drink;
+    }
+
+    public void setText(String drink) {
+        this.drink = drink;
+    }
+}
---
+++ src/test/java/StringDrinkTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringDrinkTest {
+
+    @Test
+    public void stringDrink() {
+        StringDrink drink = new StringDrink("ABCD");
+        assertEquals("ABCD", drink.getText());
+        drink.setText("DCBA");
+        assertEquals("DCBA", drink.getText());
+    }
+}
